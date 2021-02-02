d1abf9199959894b80b736b80237e0f886e15f11
Alexey Novichikhin <novichikhin.au@gmail.com>
2021-02-02 19:43:12 +0200

    upload

---
+++ template/src/Service.AssetsDictionary.Client/AssetsDictionaryClientFactory.cs
@@ -0,0 +1,20 @@
+﻿using Grpc.Net.Client;
+using JetBrains.Annotations;
+using ProtoBuf.Grpc.Client;
+using Service.AssetsDictionary.Grpc;
+
+namespace Service.AssetsDictionary.Client
+{
+    [UsedImplicitly]
+    public class AssetsDictionaryClientFactory
+    {
+        private readonly GrpcChannel _channel;
+
+        public AssetsDictionaryClientFactory(string assetsDictionaryGrpcServiceUrl)
+        {
+            _channel = GrpcChannel.ForAddress("http://localhost:5001");
+        }
+
+        public IHelloService GetHelloService() => _channel.CreateGrpcService<IHelloService>();
+    }
+}
---
+++ template/src/Service.AssetsDictionary/Settings/SettingsModel.cs
@@ -0,0 +1,11 @@
+﻿using SimpleTrading.SettingsReader;
+
+namespace Service.AssetsDictionary.Settings
+{
+    [YamlAttributesOnly]
+    public class SettingsModel
+    {
+        [YamlProperty("AssetsDictionary.SeqServiceUrl")]
+        public string SeqServiceUrl { get; set; }
+    }
+}
