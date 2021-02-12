0275ba5165f04e4db96146d7b084fb1de9f30c2d
Alexey Novichikhin <novichikhin.au@gmail.com>
2021-02-12 16:32:22 +0200

    reloaded settings

--- template/src/Service.AssetsDictionary/Program.cs
+++ template/src/Service.AssetsDictionary/Program.cs
@@ -18,6 +18,16 @@ namespace Service.AssetsDictionary
 
         public static SettingsModel Settings { get; private set; }
 
+        public static Func<T> ReloadedSettings<T>(Func<SettingsModel, T> getter)
+        {
+            return () =>
+            {
+                var settings = SettingsReader.ReadSettings<SettingsModel>(SettingsFileName);
+                var value = getter.Invoke(settings);
+                return value;
+            };
+        }
+
         public static void Main(string[] args)
         {
             Console.Title = "MyJetWallet Service.AssetsDictionary";
