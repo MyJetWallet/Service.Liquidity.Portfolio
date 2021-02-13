2fc5fe4bc9b7f1b939c30e4802774028352b898d
Alexey Novichikhin <novichikhin.au@gmail.com>
2021-02-13 02:43:31 +0200

    add grpc schema

--- template/src/Service.AssetsDictionary/Startup.cs
+++ template/src/Service.AssetsDictionary/Startup.cs
@@ -6,8 +6,10 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Autofac;
 using MyJetWallet.Sdk.GrpcMetrics;
+using MyJetWallet.Sdk.GrpcSchema;
 using Prometheus;
 using ProtoBuf.Grpc.Server;
+using Service.AssetsDictionary.Grpc;
 using Service.AssetsDictionary.Modules;
 using Service.AssetsDictionary.Services;
 using SimpleTrading.BaseMetrics;
@@ -45,7 +47,9 @@ namespace Service.AssetsDictionary
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGrpcService<HelloService>();
+                endpoints.MapGrpcSchema<HelloService, IHelloService>();
+
+                endpoints.MapGrpcSchemaRegistry();
 
                 endpoints.MapGet("/", async context =>
                 {
