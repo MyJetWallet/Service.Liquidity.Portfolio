ad1f34a37c4f3eb3d385b1406999e87a43234855
geomatika46 <alexey.p@smplt.net>
2021-07-02 16:35:36 +0300

    add tags in dto

--- src/Service.Liquidity.Portfolio.Domain.Models/ChangeBalanceHistory.cs
+++ src/Service.Liquidity.Portfolio.Domain.Models/ChangeBalanceHistory.cs
@@ -1,15 +1,17 @@
 using System;
+using System.Runtime.Serialization;
 
 namespace Service.Liquidity.Portfolio.Domain.Models
 {
+    [DataContract]
     public class ChangeBalanceHistory
     {
-        public long Id { get; set; }
-        public string BrokerId { get; set; }
-        public string ClientId { get; set; }
-        public string WalletId { get; set; }
-        public string Asset { get; set; }
-        public double VolumeDifference { get; set; }
-        public DateTime UpdateDate { get; set; }
+        [DataMember(Order = 1)] public long Id { get; set; }
+        [DataMember(Order = 2)] public string BrokerId { get; set; }
+        [DataMember(Order = 3)] public string ClientId { get; set; }
+        [DataMember(Order = 4)] public string WalletId { get; set; }
+        [DataMember(Order = 5)] public string Asset { get; set; }
+        [DataMember(Order = 6)] public double VolumeDifference { get; set; }
+        [DataMember(Order = 7)] public DateTime UpdateDate { get; set; }
     }
 }
