34af2a07a089028e3520d1dca307e7b2417563fc
geomatika46 <alexey.p@smplt.net>
2021-07-06 12:36:53 +0300

    refactoring

--- src/Service.Liquidity.Portfolio.Grpc/Models/CreateTradeManualRequest.cs
+++ src/Service.Liquidity.Portfolio.Grpc/Models/CreateTradeManualRequest.cs
@@ -6,12 +6,12 @@ namespace Service.Liquidity.Portfolio.Grpc.Models
     public class CreateTradeManualRequest
     {
         [DataMember(Order = 1)] public string BrokerId { get; set; }
-        [DataMember(Order = 3)] public string WalletName { get; set; }
-        [DataMember(Order = 4)] public string Symbol { get; set; }
-        [DataMember(Order = 6)] public double Price { get; set; }
-        [DataMember(Order = 7)] public double BaseVolume { get; set; }
-        [DataMember(Order = 8)] public double QuoteVolume { get; set; }
-        [DataMember(Order = 9)] public string Comment { get; set; }
-        [DataMember(Order = 10)] public string User { get; set; }
+        [DataMember(Order = 2)] public string WalletName { get; set; }
+        [DataMember(Order = 3)] public string Symbol { get; set; }
+        [DataMember(Order = 4)] public double Price { get; set; }
+        [DataMember(Order = 5)] public double BaseVolume { get; set; }
+        [DataMember(Order = 6)] public double QuoteVolume { get; set; }
+        [DataMember(Order = 7)] public string Comment { get; set; }
+        [DataMember(Order = 8)] public string User { get; set; }
     }
 }
