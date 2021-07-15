70b74c73ecb0f134bab32345c693501b12114ac5
geomatika46 <alexey.p@smplt.net>
2021-07-15 17:16:11 +0300

    delete trade db dto

--- src/Service.Liquidity.Portfolio.Postgres/DatabaseContext.cs
+++ src/Service.Liquidity.Portfolio.Postgres/DatabaseContext.cs
@@ -37,8 +37,7 @@ namespace Service.Liquidity.Portfolio.Postgres
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.HasDefaultSchema(Schema);
-
-            SetTradeEntity(modelBuilder);
+            
             SetBalanceEntity(modelBuilder);
             SetChangeBalanceHistoryEntity(modelBuilder);
 
@@ -70,35 +69,6 @@ namespace Service.Liquidity.Portfolio.Postgres
             modelBuilder.Entity<AssetBalance>().Property(e => e.UpdateDate);
         }
 
-        private void SetTradeEntity(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<AssetPortfolioTrade>().ToTable(TradeTableName);
-            modelBuilder.Entity<AssetPortfolioTrade>().Property(e => e.Id).UseIdentityColumn();
-            modelBuilder.Entity<AssetPortfolioTrade>().HasKey(e => e.Id);
-            modelBuilder.Entity<AssetPortfolioTrade>().Property(e => e.TradeId).HasMaxLength(64);
-            modelBuilder.Entity<AssetPortfolioTrade>().Property(e => e.AssociateBrokerId).HasMaxLength(64);
-            modelBuilder.Entity<AssetPortfolioTrade>().Property(e => e.WalletName).HasMaxLength(64);
-            modelBuilder.Entity<AssetPortfolioTrade>().Property(e => e.AssociateSymbol).HasMaxLength(64);
-            modelBuilder.Entity<AssetPortfolioTrade>().Property(e => e.BaseAsset).HasMaxLength(64);
-            modelBuilder.Entity<AssetPortfolioTrade>().Property(e => e.QuoteAsset).HasMaxLength(64);
-            modelBuilder.Entity<AssetPortfolioTrade>().Property(e => e.Side);
-            modelBuilder.Entity<AssetPortfolioTrade>().Property(e => e.Price);
-            modelBuilder.Entity<AssetPortfolioTrade>().Property(e => e.BaseVolume);
-            modelBuilder.Entity<AssetPortfolioTrade>().Property(e => e.QuoteVolume);
-            modelBuilder.Entity<AssetPortfolioTrade>().Property(e => e.BaseVolumeInUsd);
-            modelBuilder.Entity<AssetPortfolioTrade>().Property(e => e.QuoteVolumeInUsd);
-            modelBuilder.Entity<AssetPortfolioTrade>().Property(e => e.DateTime);
-            modelBuilder.Entity<AssetPortfolioTrade>().Property(e => e.ErrorMessage).HasMaxLength(256);
-            modelBuilder.Entity<AssetPortfolioTrade>().Property(e => e.Source).HasMaxLength(64);
-            modelBuilder.Entity<AssetPortfolioTrade>().Property(e => e.Comment).HasMaxLength(256);
-            modelBuilder.Entity<AssetPortfolioTrade>().Property(e => e.User).HasMaxLength(64);
-            
-            modelBuilder.Entity<AssetPortfolioTrade>().HasIndex(e => e.TradeId).IsUnique();
-            modelBuilder.Entity<AssetPortfolioTrade>().HasIndex(e => e.Source);
-            modelBuilder.Entity<AssetPortfolioTrade>().HasIndex(e => e.BaseAsset);
-            modelBuilder.Entity<AssetPortfolioTrade>().HasIndex(e => e.QuoteAsset);
-        }
-
         public static DatabaseContext Create(DbContextOptionsBuilder<DatabaseContext> options)
         {
             var activity = MyTelemetry.StartActivity($"Database context {Schema}")?.AddTag("db-schema", Schema);
@@ -119,15 +89,7 @@ namespace Service.Liquidity.Portfolio.Postgres
             ChangeBalanceHistories.Add(history);
             await SaveChangesAsync();
         }
-        
-        public async Task SaveTradesAsync(IEnumerable<AssetPortfolioTrade> trades)
-        {
-            await Trades
-                .UpsertRange(trades)
-                .On(e => e.TradeId)
-                .RunAsync();
-        }
-        
+ 
         public override void Dispose()
         {
             _activity?.Dispose();
