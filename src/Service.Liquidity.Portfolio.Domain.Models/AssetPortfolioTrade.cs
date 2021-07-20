ea46c6dd6a4f71039af601e32676653e90b335af
geomatika46 <alexey.p@smplt.net>
2021-07-20 19:20:42 +0300

    update PnlByAsset dto

--- src/Service.Liquidity.Portfolio.Domain.Models/AssetPortfolioTrade.cs
+++ src/Service.Liquidity.Portfolio.Domain.Models/AssetPortfolioTrade.cs
@@ -83,16 +83,4 @@ namespace Service.Liquidity.Portfolio.Domain.Models
         {
         }
     }
-
-    [DataContract]
-    public class PnlByAsset
-    {
-        [DataMember(Order = 1)] public string Asset { get; set; }
-        [DataMember(Order = 2)] public decimal Pnl { get; set; }
-
-        public static PnlByAsset Create(string asset, decimal pnl)
-        {
-            return new PnlByAsset() {Asset = asset, Pnl = pnl};
-        }
-    }
 }
---
+++ src/Service.Liquidity.Portfolio.Domain.Models/PnlByAsset.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.Serialization;
+
+namespace Service.Liquidity.Portfolio.Domain.Models
+{
+    [DataContract]
+    public class PnlByAsset
+    {
+        [DataMember(Order = 1)] public long TradeId { get; set; }
+        [DataMember(Order = 2)] public string Asset { get; set; }
+        [DataMember(Order = 3)] public decimal Pnl { get; set; }
+
+        public static PnlByAsset Create(string asset, decimal pnl)
+        {
+            return new PnlByAsset() {Asset = asset, Pnl = pnl};
+        }
+    }
+}
