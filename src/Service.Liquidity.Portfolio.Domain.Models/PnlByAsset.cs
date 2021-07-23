251d724004a45fa30a5ab8e0db88d029dff48008
geomatika46 <alexey.p@smplt.net>
2021-07-23 18:41:31 +0300

    delete Id field from pnl entity

--- src/Service.Liquidity.Portfolio.Domain.Models/PnlByAsset.cs
+++ src/Service.Liquidity.Portfolio.Domain.Models/PnlByAsset.cs
@@ -5,7 +5,6 @@ namespace Service.Liquidity.Portfolio.Domain.Models
     [DataContract]
     public class PnlByAsset
     {
-        public long Id { get; set; }
         [DataMember(Order = 1)] public string Asset { get; set; }
         [DataMember(Order = 2)] public decimal Pnl { get; set; }
 
